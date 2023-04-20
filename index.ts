40dbae8318950336727ebf498afaa990fcfa69ec
BillGutierrezVega <darckneck@hotmail.com>
2023-04-20 14:27:29 -0500

    resolviendo pruebas

--- index.ts
+++ index.ts
@@ -13,6 +13,25 @@ app.get('/', (req:Request, res:Response) => {
     res.send("Hola mundo desde el backend");
 });
 
+// Ruta (endpoint) devuelve data:good bye!
+app.get('/datos', (req:Request, res:Response) => {
+    res.status(200).json({
+        "data": {
+            "mensaje": "Hello world!"
+        }
+    });
+});
+
+// Ruta hello
+app.get("/hello", (req:Request, res:Response) => {
+    const name = req.query.name || "anonimo";
+    res.status(200).json({
+        "data": {
+            "mensaje": `Buenos dias ${name}`
+        }
+    });
+});
+
 // Escuchando al puerto
 app.listen(port, ()=>{
     console.log(`El puerto que se escucha es: http://localhost:${port}`);
